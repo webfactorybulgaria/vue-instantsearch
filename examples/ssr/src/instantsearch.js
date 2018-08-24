88e44583532a4af0b5a56c3738f9a1f1be7b0cf0
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-08-24 13:32:56 +0200

    chore(examples): use searchClient for SSR

--- examples/ssr/src/instantsearch.js
+++ examples/ssr/src/instantsearch.js
@@ -1,8 +1,14 @@
+import algoliasearch from 'algoliasearch/lite';
 import instantsearch from 'instantsearch.js/es';
 
-export const createInstantSearch = () =>
-  instantsearch({
-    appId: 'latency',
-    apiKey: '6be0576ff61c053d5f9a3225e2a90f76',
+export const createInstantSearch = () => {
+  const searchClient = algoliasearch(
+    'latency',
+    '6be0576ff61c053d5f9a3225e2a90f76'
+  );
+
+  return instantsearch({
     indexName: 'instant_search',
+    searchClient,
   });
+};
